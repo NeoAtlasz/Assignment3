dad9f5ef430c3c8d1fca598a2856f6ad9684bd80
NeoAtlasz <y0shii103288@gmail.com>
2025-06-18 16:51:48 +0000

    BUTTON

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("greeting").textContent = "Hi there!";
+document.getElementById("h1").textContent = "The Header";
+
+let items = document.getElementsByClassName("redclass");
+for (let i = 0; i < items.length; i++)
+{
+    items[i].textContent = "Text change";
+}
+
+const header = document.querySelector("h1");
+console.log(header);
+const red = document.querySelectorAll("redclass");
+console.log(red);
+document.getElementById("click").addEventListener("click",function()
+{
+    alert("CLICK BUTTON!");
+});
+/* "The header" */
+
+/*
+run this first: php -S localhost:3006 -t ./
+*/
